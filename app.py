727b4e8dc13d36bf0332b327f5ba3e3d45900228
Ubuntu <ubuntu@ip-172-31-40-201.ap-south-1.compute.internal>
2025-10-09 06:00:41 +0000

    change banking Title

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ html = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SBI Online Banking</title>
+    <title>Sachin & Rupali Online Banking</title>
     <style>
         body {
             font-family: Arial, sans-serif;
