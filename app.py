58bda455ad1ad882d4e6269508e64a781a536085
gitproject96 <shiwale65@gmail.com>
2025-10-09 12:18:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ html = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sachin & Rupali Online Banking</title>
+    <title>Sachin and Rupali Online Banking</title>
     <style>
         body {
             font-family: Arial, sans-serif;
